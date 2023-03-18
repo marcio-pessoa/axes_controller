72cbd325ab27f95a578f1c5b1e22ae8227cdfafb
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-03-18 10:45:12 -0300

    Fix control screen orientation

--- lib/screens/control.dart
+++ lib/screens/control.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:xc/comm.dart';
@@ -14,85 +15,86 @@ class Control extends StatefulWidget {
 }
 
 class _ControlState extends State<Control> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final Comm comm = Comm();
-
-  var _counter = 0;
-  bool _buttonPressed = false;
-  bool _loopActive = false;
 
   @override
   void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
     final cubit = context.read<BluetoothCubit>();
     comm.start(cubit.state.connection);
-    super.initState();
   }
 
   @override
   void dispose() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    DeviceOrientation.portraitUp;
     comm.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    var joystick = Row(
-      children: [
-        Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return Scaffold(
+      appBar: AppBar(title: Text(AppLocalizations.of(context)!.control)),
+      body: Row(
+        children: [
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                joypadButton("assets/arrow_up.png", 'G21', 'M00'),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        controlButton("assets/arrow_up.png", 'G21', 'M00'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        controlButton("assets/arrow_left.png", 'G41', 'M00'),
+                        controlButton("assets/stop.png", 'M00', 'M00'),
+                        controlButton("assets/arrow_right.png", 'G42', 'M00'),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        controlButton("assets/arrow_down.png", 'G22', 'M00'),
+                      ],
+                    ),
+                  ],
+                ),
+                controlButton("assets/stop.png", 'M93', ''),
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                joypadButton("assets/arrow_left.png", 'G41', 'M00'),
-                joypadButton("assets/stop.png", 'M00', 'M00'),
-                joypadButton("assets/arrow_right.png", 'G42', 'M00'),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                joypadButton("assets/arrow_down.png", 'G22', 'M00'),
-              ],
-            ),
-          ],
-        ),
-        const Padding(padding: EdgeInsets.only(right: 10)),
-        joypadButton("assets/stop.png", 'M93', 'M93'),
-      ],
-    );
-
-    return WillPopScope(
-      onWillPop: () async {
-        if (_scaffoldKey.currentState!.isDrawerOpen) {
-          Navigator.of(context).pop();
-          return false;
-        }
-        return exitDialog(context);
-      },
-      child: Scaffold(
-        appBar: AppBar(title: Text(AppLocalizations.of(context)!.control)),
-        key: _scaffoldKey,
-        drawer: const MyDrawer(),
-        body: Row(
-          children: [joystick],
-        ),
+          )
+        ],
       ),
     );
   }
 
-  Padding joypadButton(String image, String commandDown, String commandUp) {
+  Padding controlButton(String image, String commandDown, String commandUp) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Listener(
         onPointerDown: (details) => comm.send(commandDown),
         onPointerUp: (details) => comm.send(commandUp),
-        child: Image(image: AssetImage(image)),
+        child: ColorFiltered(
+            colorFilter: ColorFilter.mode(
+              Colors.teal.withOpacity(0.0),
+              BlendMode.color,
+            ),
+            child: Image(image: AssetImage(image))),
       ),
     );
   }
