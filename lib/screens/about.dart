a844bfaaf60a5d6b21ec8c24f1106333521c8c0d
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-03-19 15:26:54 -0300

    Refactor: Change bloc to method

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -1,9 +1,9 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:xc/bloc/app_info.dart' as app_info;
+import 'dart:io' show Platform;
 
-import '../components/messages.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:xc/controllers/string.dart';
 
 class About extends StatefulWidget {
   const About({Key? key}) : super(key: key);
@@ -13,77 +13,52 @@ class About extends StatefulWidget {
 }
 
 class _AboutState extends State<About> {
-  final bloc = app_info.MyBloc();
+  String appVersion = '';
 
   @override
   void initState() {
-    bloc.add(app_info.Get());
     super.initState();
-  }
-
-  @override
-  void dispose() {
-    bloc.close();
-    super.dispose();
-  }
-
-  Widget _fill() {
-    return ListView(
-      children: <Widget>[
-        ListTile(
-          title: Text(AppLocalizations.of(context)!.appVersion),
-          subtitle:
-              Text('${AppLocalizations.of(context)!.title} ${bloc.state.app}'),
-          leading: const Icon(Icons.perm_device_information_rounded),
-        ),
-        const Divider(),
-        ListTile(
-          title: Text(AppLocalizations.of(context)!.runningOn),
-          subtitle: Text(bloc.state.system),
-          leading: const Icon(Icons.auto_awesome_outlined),
-          onTap: () {
-            Navigator.of(context).pushNamed('/about/deviceDetails');
-          },
-        ),
-        const Divider(),
-        ListTile(
-          title: Text(AppLocalizations.of(context)!.legalInfo),
-          subtitle: Text(AppLocalizations.of(context)!.legalData),
-          leading: const Icon(Icons.balance_outlined),
-        ),
-      ],
-    );
+    _appInfo();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(AppLocalizations.of(context)!.about)),
-      body: BlocProvider(
-        create: (_) => bloc,
-        child: BlocListener<app_info.MyBloc, app_info.State>(
-          listener: (context, state) {
-            if (state is app_info.Error) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text(AppLocalizations.of(context)!.failedToLoadData),
-                ),
-              );
-            }
-          },
-          child: BlocBuilder<app_info.MyBloc, app_info.State>(
-            builder: (context, state) {
-              if (state is app_info.Loading) {
-                return Message.progressIndicator;
-              } else if (state is app_info.Loaded) {
-                return _fill();
-              } else {
-                return Container();
-              }
+      body: ListView(
+        children: <Widget>[
+          ListTile(
+            title: Text(AppLocalizations.of(context)!.appVersion),
+            subtitle:
+                Text('${AppLocalizations.of(context)!.title} $appVersion'),
+            leading: const Icon(Icons.perm_device_information_rounded),
+          ),
+          const Divider(),
+          ListTile(
+            title: Text(AppLocalizations.of(context)!.runningOn),
+            subtitle: Text(
+                "${Platform.operatingSystem.capitalize()}; Build/${Platform.operatingSystemVersion}"),
+            leading: const Icon(Icons.auto_awesome_outlined),
+            onTap: () {
+              Navigator.of(context).pushNamed('/about/deviceDetails');
             },
           ),
-        ),
+          const Divider(),
+          ListTile(
+            title: Text(AppLocalizations.of(context)!.legalInfo),
+            subtitle: Text(AppLocalizations.of(context)!.legalData),
+            leading: const Icon(Icons.balance_outlined),
+          ),
+        ],
       ),
     );
+  }
+
+  Future _appInfo() async {
+    final PackageInfo result = await PackageInfo.fromPlatform();
+
+    setState(() {
+      appVersion = '${result.version}, Build/${result.buildNumber}';
+    });
   }
 }
