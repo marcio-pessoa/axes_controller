ef4fd0b6180748469d2f2664b4e7bc207c963e1b
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-04-25 11:19:01 -0300

    Lint

--- lib/bluetooth_deevice_list_entry.dart
+++ lib/bluetooth_deevice_list_entry.dart
@@ -45,29 +45,32 @@ class BluetoothDeviceListEntry extends ListTile {
         );
 
   static TextStyle _computeTextStyle(int rssi) {
-    /**/ if (rssi >= -35)
+    if (rssi >= -35) {
       return TextStyle(color: Colors.greenAccent[700]);
-    else if (rssi >= -45)
+    } else if (rssi >= -45) {
       return TextStyle(
           color: Color.lerp(
               Colors.greenAccent[700], Colors.lightGreen, -(rssi + 35) / 10));
-    else if (rssi >= -55)
+    } else if (rssi >= -55) {
       return TextStyle(
           color: Color.lerp(
               Colors.lightGreen, Colors.lime[600], -(rssi + 45) / 10));
-    else if (rssi >= -65)
+    } else if (rssi >= -65) {
       return TextStyle(
           color: Color.lerp(Colors.lime[600], Colors.amber, -(rssi + 55) / 10));
-    else if (rssi >= -75)
-      return TextStyle(
-          color: Color.lerp(
-              Colors.amber, Colors.deepOrangeAccent, -(rssi + 65) / 10));
-    else if (rssi >= -85)
+    } else if (rssi >= -75) {
+      {
+        return TextStyle(
+            color: Color.lerp(
+                Colors.amber, Colors.deepOrangeAccent, -(rssi + 65) / 10));
+      }
+    } else if (rssi >= -85) {
       return TextStyle(
           color: Color.lerp(
               Colors.deepOrangeAccent, Colors.redAccent, -(rssi + 75) / 10));
-    else
+    } else {
       /*code symmetry*/
       return const TextStyle(color: Colors.redAccent);
+    }
   }
 }
