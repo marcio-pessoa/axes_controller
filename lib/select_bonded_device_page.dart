da4d827fcffe4a72b515b7f4812111fe05acdcb6
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-04-25 11:10:40 -0300

    Refactor: Remove dead code

--- lib/select_bonded_device_page.dart
+++ lib/select_bonded_device_page.dart
@@ -5,7 +5,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:xc/cubit/bluetooth_cubit.dart';
-import 'package:xc/cubit/bluetooth_state.dart';
 
 import 'bluetooth_deevice_list_entry.dart';
 
