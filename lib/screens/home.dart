4162b1b77fe49f701a4b061a1c2b8e8b98f6ccfd
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-04-26 13:01:24 -0300

    Add automatic communication interface selection (based on host platform)

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:xc/components/drawer.dart';
+import 'package:xc/cubit/comm_cubit.dart';
 import 'package:xc/screens/exit.dart';
+import 'package:xc/static/comm_interface.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,6 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    _detectHostPlatform();
     return WillPopScope(
       onWillPop: () async {
         if (_scaffoldKey.currentState!.isDrawerOpen) {
@@ -30,5 +36,15 @@ class _HomeScreenState extends State<HomeScreen> {
         body: Container(),
       ),
     );
+  }
+
+  void _detectHostPlatform() {
+    final cubit = context.read<CommCubit>();
+
+    if (Platform.isAndroid) {
+      cubit.set(commInterface: CommInterface.bluetooth);
+    } else if (Platform.isLinux) {
+      cubit.set(commInterface: CommInterface.usb);
+    }
   }
 }
