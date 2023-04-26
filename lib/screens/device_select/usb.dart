e39a1640d9bdf1438e4d24ab4b9e61d9ff745626
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-04-26 17:33:43 -0300

    Change device selection list format

--- lib/screens/device_select/usb.dart
+++ lib/screens/device_select/usb.dart
@@ -62,32 +62,39 @@ class _DeviceSelectUSBState extends State<DeviceSelectUSB> {
     ];
   }
 
-  ExpansionTile _expansionTitle(
-      address, BuildContext context, SerialPort port) {
-    return ExpansionTile(
-      title: Text(address),
-      children: [
-        CardListTile(
-          name: AppLocalizations.of(context)!.description,
-          value: port.description,
+  Widget _expansionTitle(address, BuildContext context, SerialPort port) {
+    return Card(
+      child: ListTile(
+        leading: const Icon(Icons.check_circle_outline_rounded),
+        trailing: SizedBox(
+          width: 180,
+          child: Text(address),
         ),
-        CardListTile(
-          name: AppLocalizations.of(context)!.manufacturer,
-          value: port.manufacturer,
+        title: Theme(
+          data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+          child: ExpansionTile(
+            title: Text(port.description ?? "-"),
+            children: [
+              CardListTile(
+                name: AppLocalizations.of(context)!.manufacturer,
+                value: port.manufacturer,
+              ),
+              CardListTile(
+                name: AppLocalizations.of(context)!.productName,
+                value: port.productName,
+              ),
+              CardListTile(
+                name: 'Serial Number',
+                value: port.serialNumber,
+              ),
+              CardListTile(
+                name: 'MAC Address',
+                value: port.macAddress,
+              ),
+            ],
+          ),
         ),
-        CardListTile(
-          name: AppLocalizations.of(context)!.productName,
-          value: port.productName,
-        ),
-        CardListTile(
-          name: 'Serial Number',
-          value: port.serialNumber,
-        ),
-        CardListTile(
-          name: 'MAC Address',
-          value: port.macAddress,
-        ),
-      ],
+      ),
     );
   }
 
