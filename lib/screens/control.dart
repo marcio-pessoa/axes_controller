8b373550753a96ba842be069b9a7a3584a214480
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-04-28 09:56:47 -0300

    Refactor: Move file

--- lib/screens/control.dart
+++ lib/screens/control.dart
@@ -3,7 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:wakelock/wakelock.dart';
-import 'package:xc/comm.dart';
+import 'package:xc/controllers/comm.dart';
 import 'package:xc/cubit/bluetooth_cubit.dart';
 import 'package:xc/cubit/comm_cubit.dart';
 
