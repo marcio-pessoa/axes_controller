b1dcaf192bc963d88a0b903b3829c9295a130d43
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-04-30 13:27:42 -0300

    Add clear chat button

--- lib/screens/chat/lead.dart
+++ lib/screens/chat/lead.dart
@@ -1,12 +1,20 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:xc/cubit/chat_cubit.dart';
 import 'package:xc/screens/chat/bluetooth.dart';
+import 'package:xc/static/colors.dart';
 
-class Chat extends StatelessWidget {
+class Chat extends StatefulWidget {
   const Chat({super.key});
 
+  @override
+  State<Chat> createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
   @override
   Widget build(BuildContext context) {
     Widget body = Container();
@@ -18,8 +26,48 @@ class Chat extends StatelessWidget {
     }
 
     return Scaffold(
-      appBar: AppBar(title: (Text(AppLocalizations.of(context)!.chat))),
+      appBar: AppBar(
+        title: (Text(AppLocalizations.of(context)!.chat)),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.speaker_notes_off_outlined),
+            onPressed: _clearChatDialog,
+          )
+        ],
+      ),
       body: body,
+    );
+  }
+
+  Future<void> _clearChatDialog() async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(AppLocalizations.of(context)!.chatClear),
+          content: Text(AppLocalizations.of(context)!.chatClearConfirm),
+          actions: <Widget>[
+            TextButton(
+              child: Text(
+                AppLocalizations.of(context)!.yes,
+                style: const TextStyle(color: MyColors.alert),
+              ),
+              onPressed: () {
+                final chat = context.read<ChatCubit>();
+                chat.clear();
+                setState(() {});
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text(AppLocalizations.of(context)!.no),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
