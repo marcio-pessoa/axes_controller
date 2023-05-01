be61d42f82bcc033f91b30e57f5bcac4dba5e648
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-05-01 17:37:47 -0300

    Add init and dispose methods

--- lib/controllers/comm_serial.dart
+++ lib/controllers/comm_serial.dart
@@ -1,7 +1,35 @@
+import 'dart:developer';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter_libserialport/flutter_libserialport.dart';
+import 'package:xc/cubit/comm_cubit.dart';
+import 'package:xc/cubit/serial_cubit.dart';
+import 'package:xc/static/comm_status.dart';
 
 class CommSerial {
+  SerialCubit device = SerialCubit();
+  CommCubit configuration = CommCubit();
+  CommStatus status = CommStatus.connecting;
+
+  Future<void> init(SerialCubit userDevice, CommCubit userPreferences) async {
+    device = userDevice;
+    configuration = userPreferences;
+    final address = device.state.address;
+
+    if (address?.isEmpty ?? true) {
+      log('Not connected. :-(');
+      return;
+    }
+  }
+
+  dispose() {
+    if (status == CommStatus.connected) {
+      status = CommStatus.disconnecting;
+
+      status = CommStatus.disconnected;
+    }
+  }
+
   serial() {
     String address = "/dev/ttyS4";
     SerialPort port = SerialPort(address);
@@ -22,6 +50,10 @@ class CommSerial {
       port.close();
     }
   }
+
+  Future<void> send(String text) async {}
+
+  receive(Uint8List data) {}
 
   Uint8List _stringToUinut8List(String data) {
     List<int> codeUnits = data.codeUnits;
