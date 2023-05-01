e4554cbbddc02154e61138835cd4ea719f69a2e4
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-05-01 18:39:42 -0300

    Refactor: Rename class

--- lib/screens/chat/lead.dart
+++ lib/screens/chat/lead.dart
@@ -17,7 +17,7 @@ class _ChatState extends State<Chat> {
     Widget body = Container();
 
     if (Platform.isAndroid) {
-      body = const ChatBluetooth();
+      body = const BluetoothChat();
     } else if (Platform.isLinux) {
       return const SerialChat();
     }
