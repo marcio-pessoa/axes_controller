c597fa83d21fd357c30899fb886f0f1845a733de
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-05-01 18:14:48 -0300

    Remove unused property

--- lib/cubit/comm_state.dart
+++ lib/cubit/comm_state.dart
@@ -4,11 +4,9 @@ import 'package:xc/static/end_line.dart';
 class CommState {
   final EndLine endLine;
   final CommInterface interface;
-  final String? address;
 
   CommState({
     required this.endLine,
     required this.interface,
-    required this.address,
   });
 }
