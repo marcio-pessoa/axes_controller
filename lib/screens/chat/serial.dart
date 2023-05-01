1242e5842477b9826e81ee1f1cfad8c2f149a410
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-05-01 20:42:01 -0300

    Add clear chat

--- lib/screens/chat/serial.dart
+++ lib/screens/chat/serial.dart
@@ -4,8 +4,10 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:xc/components/comm_status_icon.dart';
 import 'package:xc/controllers/comm_serial.dart';
 import 'package:xc/controllers/hint_text.dart';
+import 'package:xc/cubit/chat_cubit.dart';
 import 'package:xc/cubit/comm_cubit.dart';
 import 'package:xc/cubit/serial_cubit.dart';
+import 'package:xc/static/colors.dart';
 import 'package:xc/static/comm_status.dart';
 
 class SerialChat extends StatefulWidget {
@@ -34,6 +36,8 @@ class _SerialChatState extends State<SerialChat> {
 
   @override
   Widget build(BuildContext context) {
+    final chat = context.read<ChatCubit>();
+
     String hint = hintText(context, comm.port.name.toString(), comm.status);
 
     return Scaffold(
@@ -41,10 +45,10 @@ class _SerialChatState extends State<SerialChat> {
         title: (Text(AppLocalizations.of(context)!.chat)),
         actions: <Widget>[
           CommStatusIcon(status: comm.status),
-          // IconButton(
-          //   icon: const Icon(Icons.speaker_notes_off_outlined),
-          //   onPressed: _clearChatDialog,
-          // )
+          IconButton(
+            icon: const Icon(Icons.speaker_notes_off_outlined),
+            onPressed: _clearChatDialog,
+          )
         ],
       ),
       body: Column(
@@ -112,5 +116,37 @@ class _SerialChatState extends State<SerialChat> {
 
   void _receive(event) {
     debugPrint('Read data: $event');
+  }
+
+  Future<void> _clearChatDialog() async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(AppLocalizations.of(context)!.chatClear),
+          content: Text(AppLocalizations.of(context)!.chatClearConfirm),
+          actions: <Widget>[
+            TextButton(
+              child: Text(
+                AppLocalizations.of(context)!.yes,
+                style: const TextStyle(color: MyColors.alert),
+              ),
+              onPressed: () {
+                final chat = context.read<ChatCubit>();
+                chat.clear();
+                setState(() {});
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text(AppLocalizations.of(context)!.no),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
