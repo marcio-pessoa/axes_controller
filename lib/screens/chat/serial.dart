7c7e68c2b450ca2d199619b9dfa1aef70d7ce1c4
Márcio Pessoa <marcio.pessoa@gmail.com>
2023-05-01 16:32:01 -0300

    Add basic serial functions

---
+++ lib/screens/chat/serial.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_libserialport/flutter_libserialport.dart';
+
+serial() {
+  String address = "/dev/ttyS4";
+  SerialPort port = SerialPort(address);
+  port.openReadWrite();
+
+  try {
+    debugPrint("Writen bytes: ${port.write(_stringToUinut8List("Hello"))}");
+    SerialPortReader reader = SerialPortReader(port);
+    Stream<String> upcommingData = reader.stream.map((event) {
+      return String.fromCharCodes(event);
+    });
+
+    upcommingData.listen((event) {
+      debugPrint('Read data: $event');
+    });
+  } on SerialPortError catch (err, _) {
+    debugPrint(SerialPort.lastError.toString());
+    port.close();
+  }
+}
+
+Uint8List _stringToUinut8List(String data) {
+  List<int> codeUnits = data.codeUnits;
+  Uint8List uint8list = Uint8List.fromList(codeUnits);
+  return uint8list;
+}
