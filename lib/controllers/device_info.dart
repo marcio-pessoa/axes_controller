197b91a4fcb211d31c47fc130359efe45ac7258f
Márcio Pessoa <marcio.pessoa@gmail.com>
2024-01-07 00:55:17 -0300

    Fix describeEnum deprecation
    'describeEnum' is deprecated and shouldn't be used. Use the `name` getter on enums instead. This feature was deprecated after v3.10.0-1.1.pre.
    Try replacing the use of the deprecated member with the replacement.

--- lib/controllers/device_info.dart
+++ lib/controllers/device_info.dart
@@ -111,7 +111,7 @@ class DeviceInfo {
 
   Map<String, dynamic> _readWebBrowserInfo(WebBrowserInfo data) {
     return <String, dynamic>{
-      'browserName': describeEnum(data.browserName),
+      'browserName': data.browserName.name,
       'appCodeName': data.appCodeName,
       'appName': data.appName,
       'appVersion': data.appVersion,
